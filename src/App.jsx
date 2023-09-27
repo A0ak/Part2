56e42fd6671b038ad08c8d29d83f5e9e07dc2fce
Abdullah <hak.99.abdullah.0@gmail.com>
2023-09-27 07:20:11 +0300

    ülkeler son kommit

--- src/App.jsx
+++ src/App.jsx
@@ -1,133 +1,38 @@
 import { useState, useEffect } from 'react'
-import personService from './api'
-import './index.css';
-
+import countryService from './api'
 
 const App = () => {
-  const [persons, setPersons] = useState([])
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [notification, setNotification] = useState(null);
+  const [countries, setCountries] = useState([])
   const [search, setSearch] = useState('');
 
-
   useEffect(() => {
-    personService.getAll().then(response => {
-      setPersons(response.data)
+    countryService.getAll().then(response => {
+      if (Array.isArray(response.data)) {
+        setCountries(response.data)
+      } else {
+        console.error('Data is not an array:', response.data);
+      }
     })
   }, [])
-
-
-  const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
-
-
-  const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
-  }
-
 
   const handleSearchChange = (event) => {
     setSearch(event.target.value);
   }
 
-
-  const addName = (event) => {
-    event.preventDefault()
-    const nameObject = {
-      name: newName,
-      number: newNumber,
-      id: persons.length + 1,
-    }
-
-    const existingPerson = persons.find(person => person.name === newName)
-
-    if (existingPerson) {
-      if (window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)) {
-        personService.update(existingPerson.id, nameObject)
-          .then(response => {
-            setPersons(persons.map(person => person.id !== existingPerson.id ? person : response.data))
-            setNewName('')
-            setNewNumber('')
-            setNotification({ message: `Updated ${newName}`, type: 'success' });
-            setTimeout(() => {
-              setNotification(null);
-            }, 5000);
-          })
-          .catch(error => {
-            setNotification({ message: `The information of ${newName} has already been removed from server`, type: 'error' });
-            setTimeout(() => {
-              setNotification(null);
-            }, 5000);
-          })
-      }
-    } else {
-      personService.create(nameObject)
-        .then(response => {
-          setPersons(persons.concat(response.data))
-          setNewName('')
-          setNewNumber('')
-          setNotification({ message: `Added ${newName}`, type: 'success' });
-          setTimeout(() => {
-            setNotification(null);
-          }, 5000);
-        })
-        .catch(error => {
-          setNotification({ message: `Failed to add ${newName}`, type: 'error' });
-          setTimeout(() => {
-            setNotification(null);
-          }, 5000);
-        })
-    }
-  }
-
-
-  const deleteName = (id) => {
-    const personToDelete = persons.find(person => person.id === id);
-    if (window.confirm("Do you really want to delete this person?")) {
-      personService.deletePerson(id)
-        .then(() => {
-          setPersons(persons.filter(person => person.id !== id))
-          setNotification({ message: `Deleted ${personToDelete.name}`, type: 'success' });
-          setTimeout(() => {
-            setNotification(null);
-          }, 5000);
-        })
-    }
-  };
-
-
-  const filteredPersons = persons.filter(person =>
-    person.name.toLowerCase().includes(search.toLowerCase())
+  const filteredCountries = countries.filter(country =>
+    country.name.common.toLowerCase().includes(search.toLowerCase())
   );
-
 
   return (
     <div>
-      <h2>Phonebook</h2>
-      {notification && <div className={`notification ${notification.type}`}>{notification.message}</div>}
+      <h2>Country Info</h2>
       <div>
-        Search: <input value={search} onChange={handleSearchChange} /> {/* arama alanını ekle */}
+        Search: <input value={search} onChange={handleSearchChange} />
       </div>
-      <h2>Add a new</h2>
-      <form onSubmit={addName}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNumberChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
       <div>
-        {filteredPersons.map((person) =>
-          <p key={person.id}>
-            {person.name} {person.number}
-            <button onClick={() => deleteName(person.id)}>delete</button>
+        {filteredCountries.map((country) =>
+          <p key={country.name.common}>
+            {country.name.common}
           </p>
         )}
       </div>
@@ -135,7 +40,4 @@ const App = () => {
   )
 }
 
-
 export default App
-
-
--- src/api.js
+++ src/api.js
@@ -1,29 +1,9 @@
 import axios from 'axios'
 
-
-const baseUrl = 'http://localhost:3001/persons'
-
+const baseUrl = 'https://restcountries.com/v3.1/all'
 
 const getAll = () => {
     return axios.get(baseUrl)
 }
 
-
-const create = (newObject) => {
-    return axios.post(baseUrl, newObject)
-}
-
-
-const deletePerson = (id) => {
-    return axios.delete(`${baseUrl}/${id}`)
-}
-
-
-const update = (id, newObject) => {
-    return axios.put(`${baseUrl}/${id}`, newObject)
-}
-
-
-export default { getAll, create, deletePerson, update }
-
-
+export default { getAll }
